4cb6af22d7e141c8cf0f234db0957d3ec9537c71
Lizz-Reid <139030289+Lizz-Reid@users.noreply.github.com>
2024-01-17 09:39:10 +1100

    Dropdown complete

--- index.js
+++ index.js
@@ -26,3 +26,21 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+// skills
+
+document.querySelectorAll('.accordion-item h4').forEach((accordionToggle) => { 
+  accordionToggle.addEventListener('click', () => { 
+  const accordionItem = accordionToggle.parentNode; 
+  const accordionContent = accordionToggle.nextElementSibling; 
+
+        // If this accordion item is already open, close it.
+   if (accordionContent.style.maxHeight) { 
+       accordionContent.style.maxHeight = null; 
+       accordionItem.classList.remove('active'); 
+      } else {
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px'; 
+            accordionItem.classList.add('active'); 
+        }
+    });
+});
